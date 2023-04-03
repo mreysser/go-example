c529703a60fd1b0f4364b2b703372502bbf553ae
Manfred Reysser <mreysser@gmail.com>
2023-04-02 18:27:58 -0700

    feat: initial lifecycle handling

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"context"
 	"net/http"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"github.com/mreysser/go-example/handler"
+	"github.com/mreysser/go-example/lifecycle"
 )
 
 func init() {
+	// TODO: import from env
 	log.SetLevel(log.DEBUG)
 }
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	token := lifecycle.InitializeLifecycle(context.Background(), []syscall.Signal{syscall.SIGTERM})
 
 	e := echo.New()
 
@@ -25,8 +27,10 @@ func main() {
 	e.GET("/", handler.Hello)
 
 	go runServer(e)
+	token.RegisterShutdownHandler(func() { e.Shutdown(token.Ctx) })
 
-	<-ctx.Done()
+	<-token.Ctx.Done()
+	log.Warn("application exit")
 }
 
 func runServer(e *echo.Echo) {
