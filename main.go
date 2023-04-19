936f770c79d0abbff06a6b460f3ec0f732dbcee8
Manfred Reysser <mreysser@gmail.com>
2023-04-19 11:09:53 -0700

    chore: switch lifecycle to github package

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	lifecycle "github.com/mreysser/go-example/lifecycle"
+	lifecycle "github.com/mreysser/go-lifecycle"
 	log "github.com/sirupsen/logrus"
 )
 
